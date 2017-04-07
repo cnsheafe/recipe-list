03919e7ab5f3b47516b84b38627176a7e2e15ea6
cnsheafe <cnsheafe@gmail.com>
2017-04-07 15:16:22 -0500

    added food2fork api query

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 
-
-
+// TODO: Get searchbar working
+//jshint esversion: 6
+function getFood2ForkResults(query, sortByRating=true, pageCount=1) {
+  let settings = {
+    url: 'http://food2fork.com/api/search',
+    data: {
+      key: '488b157b5f1a04456443b2378a56542d',
+      q: query,
+      sort:  sortOrder,
+      page:  pageCount
+    },
+    dataType: 'json'
+  };
+  return $.ajax(settings);
+}
 
 $(function main() {
-
+  $('#search-bar').on('submit',function(event) {
+    event.preventDefault();
+    let query = $(this).find('input').text();
+    let xhrPromise = getFood2ForkResults(query);
+    xhrPromise.done(function (data) {
+      
+    });
+  });
 });
