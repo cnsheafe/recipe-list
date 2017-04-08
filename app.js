fde2353e116f147040bc9bb752ef72bfba51031d
cnsheafe <cnsheafe@gmail.com>
2017-04-07 19:30:16 -0500

    added code for search result rendering

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 // TODO: Get searchbar working
 //jshint esversion: 6
-function getFood2ForkResults(query, sortByRating=true, pageCount=1) {
+function getFood2ForkResults(query, sortOrder='r', pageCount=1) {
   let settings = {
     url: 'http://food2fork.com/api/search',
     data: {
@@ -10,9 +10,30 @@ function getFood2ForkResults(query, sortByRating=true, pageCount=1) {
       sort:  sortOrder,
       page:  pageCount
     },
-    dataType: 'json'
+    dataType: 'json',
+    crossDomain: true,
+    xhrFields: {
+      withCredentials: true
+    }
   };
   return $.ajax(settings);
+}
+
+function getPreviewData(searchResult) {
+  let recipeObj = searchResult.recipe;
+  let previewInfo = {
+    imgUrl: recipeObj.image_url,
+    title: recipeObj.title,
+    id: recipeObj.f2f_url.split('/').pop(),
+    srcUrl: source_url
+  };
+  return previewInfo;
+}
+
+function renderPreview(previewInfo) {
+  let imgHtml = '<img src="'+previewInfo.imgUrl+'">';
+  let titleHtml = '<span> '+previewInfo.title+'</span>';
+  $('#search-results').append('<li>'+imgHtml+titleHtml+'</li>');
 }
 
 $(function main() {
@@ -20,8 +41,8 @@ $(function main() {
     event.preventDefault();
     let query = $(this).find('input').text();
     let xhrPromise = getFood2ForkResults(query);
-    xhrPromise.done(function (data) {
-      
+    xhrPromise.done(function(data) {
+      console.log(data);
     });
   });
 });
