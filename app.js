17f1493269a78b804a83d0f3d7dab0c7076ad968
cnsheafe <cnsheafe@gmail.com>
2017-04-09 18:06:27 -0500

    removed unused functions

--- app.js
+++ app.js
@@ -62,23 +62,6 @@ function renderResultsList(previewArray){
   $('#search-results').html(previewHtml);
 }
 
-function getPreviewData(searchResult) {
-  let recipeObj = searchResult.recipe;
-  let previewInfo = {
-    imgUrl: recipeObj.image_url,
-    title: recipeObj.title,
-    id: recipeObj.f2f_url.split('/').pop(),
-    srcUrl: source_url
-  };
-  return previewInfo;
-}
-
-function renderPreview(previewInfo) {
-  let imgHtml = '<img src="'+previewInfo.imgUrl+'">';
-  let titleHtml = '<span> '+previewInfo.title+'</span>';
-  $('#search-results').append('<li>'+imgHtml+titleHtml+'</li>');
-}
-
 $(function main() {
   $('#search-form').on('submit', function(event) {
     event.preventDefault();
