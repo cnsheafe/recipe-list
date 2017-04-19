ed97fbe99d08084b81a891afe36f5f95cd2af5fd
cnsheafe <cnsheafe@gmail.com>
2017-04-18 23:41:00 -0500

    Accomodates html changes and styling

--- src/scripts/modules/view/render-generic.js
+++ src/scripts/modules/view/render-generic.js
@@ -3,11 +3,13 @@ export function showRecipe($selector, recipe) {
   console.log(recipe);
   let recipeInfo ='<span><h1>'+
     recipe.title+'</h1>'+
-    '<h2>'+recipe.readyInMinutes+'minutes</h2></span><ul>';
+    '<h2>'+recipe.readyInMinutes+' minutes</h2></span>'+
+    '<h3>Ingredients</h3><ul>';
   $.each(recipe.listofIngredients, function(ind, obj) {
-    recipeInfo += '<li>'+obj.amount+obj.name+'</li>';
+    recipeInfo += '<li><input type="checkbox">'+obj.amount+
+    ' '+obj.name+'</li>';
   });
-  recipeInfo += '</ul><ol>';
+  recipeInfo += '</ul><h3>Directions</h3><ol>';
   $.each(recipe.instructions, function(ind,val) {
     recipeInfo += '<li>'+val+'</li>';
   });
--- src/scripts/modules/view/search-results.js
+++ src/scripts/modules/view/search-results.js
@@ -3,10 +3,10 @@ export function showSearchResults(state){
   let previewHtml = '';
   $.each(state.resultList, function(ind, obj) {
     previewHtml += '<li data-recipeid="'+obj.id+'"><a>'+
-    '<img src="'+obj.imgUrl+'" alt="'+obj.title+'">'+
-    '<span>'+obj.title+'</span>'+
+    '<img src="'+obj.imgUrl+'" alt="'+obj.title+'"></a>'+
+    '<span><p>'+obj.title+'</p></span>'+
     // '<form><input type="submit"></form>'+
-    '</li></a>';
+    '</li>';
   });
   $('#search-results').html(previewHtml);
 }
