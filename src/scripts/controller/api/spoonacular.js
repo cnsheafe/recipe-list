67fcb40f263c22cfc7ccb252767270ceb368beb9
cnsheafe <cnsheafe@gmail.com>
2017-05-03 17:00:47 -0500

    Supporting a better endpoint

--- src/scripts/controller/api/spoonacular.js
+++ src/scripts/controller/api/spoonacular.js
@@ -2,22 +2,8 @@
 // See Spoonacular API documentation
 const MASHAPE_KEY = 'AyBmxPBKYUmshcuDOEgra2staJv9p1Tm8cgjsnsk5j9j5dONbK';
 
-export function getSearchResults(userQuery) {
-  let ajaxSettings = {
-    url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/site/search',
-    data: {
-      query: userQuery
-    },
-    dataType: 'json',
-    headers: {
-      'X-Mashape-Key': MASHAPE_KEY,
-      Accept: 'application/json'
-    }
-  };
-  return $.ajax(ajaxSettings);
-}
 
-export function getBetterSearchResults(userQuery){
+export function getSearchResults(userQuery){
   let ajaxSettings = {
     url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search',
     data: {
