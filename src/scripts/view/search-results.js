ef29b59e9a4890d2ca66494e29e9b8dceabd1d38
cnsheafe <cnsheafe@gmail.com>
2017-05-03 22:52:14 -0500

    Made responsive and using string-literal templates

--- src/scripts/view/search-results.js
+++ src/scripts/view/search-results.js
@@ -3,9 +3,17 @@ export function showSearchResults(state){
   let previewHtml = '';
   $.each(state.resultList, (ind, result) => {
     previewHtml +=
-    `<li data-recipeid=${result.id}>
-      <a><img src=${state.baseImgUri}${result.image} alt=${result.title}></a>
-      <span><p>${result.title}</p></span>
+    `<li class="row" data-recipeid=${result.id}>
+      <span class="col-xs-4 ">
+        <img src=${state.baseImgUri}${result.image}
+        class="img-responsive"
+        alt="${result.title}"></img>
+      </span>
+      <span class="col-xs-8 result-preview">
+        <h3>${result.title}</h3>
+        <span class="glyphicon glyphicon-time"></span>
+        <span>${result.readyInMinutes}</span>
+      </span>
     </li>`;
   });
   $('#search-results').html(previewHtml);
