792c796b61e450bc5416eb6767dc539391e36fa1
cnsheafe <cnsheafe@gmail.com>
2017-05-03 20:35:06 -0500

    using string templates

--- src/scripts/view/search-results.js
+++ src/scripts/view/search-results.js
@@ -1,11 +1,12 @@
 // jshint esversion: 6
 export function showSearchResults(state){
   let previewHtml = '';
-  $.each(state.resultList, function(ind, obj) {
-    previewHtml += '<li data-recipeid="'+obj.id+'"><a>'+
-    '<img src="'+obj.imgUrl+'" alt="'+obj.title+'"></a>'+
-    '<span><p>'+obj.title+'</p></span>'+
-    '</li>';
+  $.each(state.resultList, (ind, result) => {
+    previewHtml +=
+    `<li data-recipeid=${result.id}>
+      <a><img src=${state.baseImgUri}${result.image} alt=${result.title}></a>
+      <span><p>${result.title}</p></span>
+    </li>`;
   });
   $('#search-results').html(previewHtml);
 }
