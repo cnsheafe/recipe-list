f7da8bd1e508db1dcd322977050fc062ea2eb791
cnsheafe <cnsheafe@gmail.com>
2017-05-04 15:47:26 -0500

    Changed directory

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 // jshint esversion:6
 export default {
-  entry: 'src/scripts/modules/controller/app.js',
+  entry: 'src/scripts/controller/app.js',
   dest: 'build/js/app.min.js',
   format: 'iife',
   sourceMap: 'inline'
